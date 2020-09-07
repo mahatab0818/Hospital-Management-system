3a22f70b5c15e518be936f9efa3e51edc5d7c0fc
mahatab0818 <65038252+mahatab0818@users.noreply.github.com>
2020-09-07 14:13:47 +0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">American Hospital</a>
+    </div>
+	
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Patient</a></li>
+      <li><a href="#">Doctor</a></li>
+    </ul>
+	
+	<ul class="nav navbar-nav navbar-right">
+	<li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+	<li><a href="#"><span class="glyphicon glyphicon-user"></span>Log Out</a></li>
+     
+    </ul>
+  </div>
+</nav>
